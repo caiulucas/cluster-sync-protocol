8321a5a64a65d698df8c52d32c4986246cd175d3
Vitor Oliveira Diniz <vitor.oliveira.vod@gmail.com>
2024-09-09 13:07:55 -0300

    started clusterElement class

--- cluster_element.py
+++ cluster_element.py
@@ -1,2 +1,24 @@
-class clusterElement():
+class ClusterElement:
+    def __init__(self, id: int, port:int, ip:str):
+        self.id = id
+        self.port = port
+        self.ip = ip
+        self.clusterSocketsList
+        self.clusterElementList
+
+
+class ClientInfo:
+    def __init__(self, id: int, ip: str, port: int):
+        self.id = id
+        self.ip = ip
+        self.port = port
+
+
+class ElementInfo:
+    def __init__(self, id: int, ip: str, port: int):
+        self.id = id
+        self.ip = ip
+        self.port = port
+
+
     
